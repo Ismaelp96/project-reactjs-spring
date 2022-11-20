0f233b8309927c61dfa27915d3baf8e857decd9e
Ismaelp96 <ismael6991@live.com>
2022-11-20 09:10:23 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import CardVendas from "./components/CardVendas";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
